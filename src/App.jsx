d0b62d7dae415c475cbbc54837e4a944c9b79926
Diego Galvão <diegohenriquegalvao@gmail.com>
2023-09-30 16:44:07 -0300

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignOut from "./pages/SignOut";
+import SignIn from "./pages/SignIn";
+import About from "./pages/About";
 
 export default function App() {
   return (
-<div className='text-red-500'>App</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/signout" element={<SignOut />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
