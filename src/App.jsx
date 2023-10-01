45d3fdb3ebf373efc1a52904befc4a3edff7d67f
Diego Galvão <diegohenriquegalvao@gmail.com>
2023-09-30 23:40:33 -0300

    signup page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
-import SignOut from "./pages/SignOut";
 import SignIn from "./pages/SignIn";
 import About from "./pages/About";
 import Header from "./components/Header";
+import SignUp from "./pages/SignUp";
 
 export default function App() {
   return (
@@ -13,7 +13,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/signout" element={<SignOut />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/about" element={<About />} />
         <Route path="/signin" element={<SignIn />} />
       </Routes>
