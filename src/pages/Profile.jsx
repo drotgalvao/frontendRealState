aa0843cfb80108379030495e4489404573a6d0d7
Diego <diegohenriquegalvao@gmail.com>
2023-10-16 13:22:32 -0300

    create list completed

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -18,6 +18,7 @@ import {
   signoutSuccess,
   signoutFailure,
 } from "../redux/user/userSlice";
+import { Link } from "react-router-dom";
 
 const Profile = () => {
   const { currentUser, loading, error, success } = useSelector(
@@ -193,6 +194,9 @@ const Profile = () => {
         >
           {loading ? "Loading..." : "Update"}
         </button>
+        <Link className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95" to={"/create-listing"}>
+          Create Listing
+        </Link>
       </form>
 
       <div className="flex justify-between mt-5">
