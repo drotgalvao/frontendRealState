602ee997ec409af0674f9b95521f4a84fa9c5aad
Diego <diegohenriquegalvao@gmail.com>
2023-10-21 18:31:35 -0300

    Update listing page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -161,17 +161,16 @@ const Profile = () => {
       const data = await res.json();
       if (data.success === false) {
         console.log(data.message);
-        return
+        return;
       }
-      
+
       setUserListings((prev) => {
         prev.filter((listing) => listing._id !== id);
-      })
-
+      });
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   return (
     <div className="p-3 max-w-lg mx-auto">
@@ -267,27 +266,45 @@ const Profile = () => {
         {showListingsError ? "You have no listings" : ""}
       </p>
 
-      {userListings &&
-        userListings.length > 0 &&
+      {userListings && userListings.length > 0 && (
         <div className="flex flex-col gap-4">
-          <h1 className="text-center mt-7 text-2xl font-semibold">Your Listings</h1>
+          <h1 className="text-center mt-7 text-2xl font-semibold">
+            Your Listings
+          </h1>
           {userListings.map((listing) => (
-            <div key={listing._id} className="border rounded-lg p-3 flex justify-between items-center gap-4">
+            <div
+              key={listing._id}
+              className="border rounded-lg p-3 flex justify-between items-center gap-4"
+            >
               <Link to={`/listing/${listing._id}`}>
-                <img src={listing.imageUrls[0]} alt="listing image" className="h-16 w-16 object-contain"></img>
+                <img
+                  src={listing.imageUrls[0]}
+                  alt="listing image"
+                  className="h-16 w-16 object-contain"
+                ></img>
               </Link>
-              <Link className="text-slate-700 font-semibold hover:underline truncate flex-1" to={`/listing/${listing._id}`}>
+              <Link
+                className="text-slate-700 font-semibold hover:underline truncate flex-1"
+                to={`/listing/${listing._id}`}
+              >
                 <p>{listing.title}</p>
               </Link>
-  
+
               <div className="flex flex-col items-center">
-                <button onClick={() => handleDeleteListing(listing._id)} className="text-red-700 uppercase">Delete</button>
-                <button className="text-green-700 uppercase">Edit</button>
+                <button
+                  onClick={() => handleDeleteListing(listing._id)}
+                  className="text-red-700 uppercase"
+                >
+                  Delete
+                </button>
+                <Link to={`/update-listing/${listing._id}`}>
+                  <button className="text-green-700 uppercase">Edit</button>
+                </Link>
               </div>
-  
             </div>
           ))}
-        </div>}
+        </div>
+      )}
     </div>
   );
 };
