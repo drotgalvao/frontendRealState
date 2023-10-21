b2f764b1e77b64af13f843afa7804f341f1ccd9f
Diego <diegohenriquegalvao@gmail.com>
2023-10-21 16:34:09 -0300

    Listings show completed

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -29,6 +29,9 @@ const Profile = () => {
   const [uploadProgress, setUploadProgress] = useState(0);
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
+  const [updateSucess, setUpdateSucess] = useState(false);
+  const [showListingsError, setShowListingsError] = useState(false);
+  const [userListings, setUserListings] = useState([]);
 
   const dispatch = useDispatch();
 
@@ -96,6 +99,7 @@ const Profile = () => {
       } else {
         dispatch(updateUserSuccess(data));
       }
+      setUpdateSucess(true);
     } catch (error) {
       dispatch(updateUserFailure(error.message));
     }
@@ -130,6 +134,22 @@ const Profile = () => {
       }
     } catch (error) {
       dispatch(signoutFailure(error.message));
+    }
+  };
+
+  const handleShowListing = async () => {
+    try {
+      setShowListingsError(false);
+      const res = await fetch(`/api/user/listings/${currentUser._id}`);
+      const data = await res.json();
+      if (data.success === false) {
+        setShowListingsError(true);
+      } else {
+        setShowListingsError(false);
+        setUserListings(data);
+      }
+    } catch (error) {
+      setShowListingsError(true);
     }
   };
 
@@ -194,7 +214,10 @@ const Profile = () => {
         >
           {loading ? "Loading..." : "Update"}
         </button>
-        <Link className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95" to={"/create-listing"}>
+        <Link
+          className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95"
+          to={"/create-listing"}
+        >
           Create Listing
         </Link>
       </form>
@@ -213,7 +236,38 @@ const Profile = () => {
 
       <p className="text-red-700 mt-5">{error ? error : ""}</p>
 
-      <p className="text-green-700 mt-5">{success ? "Profile Updated" : ""}</p>
+      <p className="text-green-700 mt-5">
+        {updateSucess ? "Profile Updated" : ""}
+      </p>
+
+      <button onClick={handleShowListing} className="text-green-700 w-full">
+        Show Listings
+      </button>
+      <p className="text-red-700 mt-5">
+        {showListingsError ? "You have no listings" : ""}
+      </p>
+
+      {userListings &&
+        userListings.length > 0 &&
+        <div className="flex flex-col gap-4">
+          <h1 className="text-center mt-7 text-2xl font-semibold">Your Listings</h1>
+          {userListings.map((listing) => (
+            <div key={listing._id} className="border rounded-lg p-3 flex justify-between items-center gap-4">
+              <Link to={`/listing/${listing._id}`}>
+                <img src={listing.imageUrls[0]} alt="listing image" className="h-16 w-16 object-contain"></img>
+              </Link>
+              <Link className="text-slate-700 font-semibold hover:underline truncate flex-1" to={`/listing/${listing._id}`}>
+                <p>{listing.title}</p>
+              </Link>
+  
+              <div className="flex flex-col items-center">
+                <button className="text-red-700 uppercase">Delete</button>
+                <button className="text-green-700 uppercase">Edit</button>
+              </div>
+  
+            </div>
+          ))}
+        </div>}
     </div>
   );
 };
